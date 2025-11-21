a42707dcec4ec8c1d5d8a201f831f0dae3f1e9f6
Wilsonn06 <161280166+Wilsonn06@users.noreply.github.com>
2025-11-22 02:27:45 +0700

    Update UnitDetailApp.jsx

--- src/components_app/UnitDetailApp.jsx
+++ src/components_app/UnitDetailApp.jsx
@@ -13,7 +13,7 @@ export default function UnitDetailApp() {
 
   const fetchUnitDetail = async () => {
     try {
-      const res = await fetch(`http://s-rusun-app:3002/unit/${unit_id}`);
+      const res = await fetch(`http://localhost/app/unit/${unit_id}`);
       const data = await res.json();
 
       if (!res.ok) {
