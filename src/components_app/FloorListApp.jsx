0da1b1c8e59a0b3d762117d7e8576fefb3d79ec7
Wilsonn06 <161280166+Wilsonn06@users.noreply.github.com>
2025-11-23 22:32:18 +0700

    Update FloorListApp.jsx

--- src/components_app/FloorListApp.jsx
+++ src/components_app/FloorListApp.jsx
@@ -8,7 +8,7 @@ export default function FloorListApp() {
   useEffect(() => {
     const fetchFloors = async () => {
       try {
-        const res = await fetch(`http://localhost:8080/app/floor`);
+        const res = await fetch(`${import.meta.env.VITE_API_BASE}/app/floor`);
         const data = await res.json();
 
         if (!res.ok) setError("Gagal mengambil floor");
