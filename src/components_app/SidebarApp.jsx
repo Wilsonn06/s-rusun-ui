c8881991d622be5295eb7c270f905df54614ba8b
Wilsonn06 <161280166+Wilsonn06@users.noreply.github.com>
2025-11-28 21:05:44 +0700

    Update SidebarApp.jsx

--- src/components_app/SidebarApp.jsx
+++ src/components_app/SidebarApp.jsx
@@ -9,7 +9,6 @@ export default function SidebarApp() {
     { path: '/app-ui/flat', label: 'Rumah Susun' },
     { path: '/app-ui/tower', label: 'Tower' },
     { path: '/app-ui/floor', label: 'Lantai' },
-    
   ];
 
   return (
@@ -17,10 +16,12 @@ export default function SidebarApp() {
       <div className="sidebar-title">S-Rusun App</div>
       <ul className="nav-list">
         {menus.map((menu) => (
-          <li key={menu.path}>
+          <li className="nav-item" key={menu.path}>
             <Link
               to={menu.path}
-              className={`nav-link ${location.pathname.startsWith(menu.path) ? 'active' : ''}`}
+              className={`nav-link ${
+                location.pathname.startsWith(menu.path) ? 'active' : ''
+              }`}
             >
               {menu.label}
             </Link>
