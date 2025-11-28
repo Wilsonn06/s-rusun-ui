ca80bc129309ee8e890f0f194968ca166f1e7694
Wilsonn06 <161280166+Wilsonn06@users.noreply.github.com>
2025-11-28 09:22:04 +0700

    Update PemilikEdit.jsx

--- src/components/PemilikEdit.jsx
+++ src/components/PemilikEdit.jsx
@@ -41,7 +41,7 @@ export default function PemilikEdit() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_BASE}/adm/pemilik/${pemilik_id}`, {
+      const res = await fetch(`${API_BASE}/adm/pemilik/${pemilik_id}`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
